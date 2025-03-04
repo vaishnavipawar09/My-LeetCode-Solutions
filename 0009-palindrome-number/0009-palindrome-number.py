9ad918f8d833fc04f48368ec328566e64b85f0e2
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-03-04 15:35:25 -0500

    Time: 15 ms (19.38%), Space: 18.1 MB (19.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,31 +1,12 @@
-"""class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-
-        rev=0
-        pal = x
-        while x !=0:
-            lastdigit = x % 10
-            rev = rev * 10 + lastdigit
-            x = x // 10
-        return pal == rev"""
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return False 
-            
-        org = x
-        rev = 0
-        while x != 0:
-            rem = x % 10
-            rev = rev * 10 + rem 
+            return False
+        rev=0
+        pal = x
+        while x !=0:
+            ld = x % 10
+            rev = rev * 10 + ld
             x = x // 10
-        
-        return org == rev
+        return pal == rev
 
-       
-
-
-        
