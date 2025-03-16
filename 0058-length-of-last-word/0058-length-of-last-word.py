284c20e2e1cbb021f2759c83178376cf775b4e72
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-03-16 16:53:58 -0400

    Time: 3 ms (4.44%), Space: 17.7 MB (88.45%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = i = 0
+        while i < len(s):
+            if s[i] == ' ':
+                while i < len(s) and s[i] == ' ':
+                    i += 1
+                if i == len(s):
+                    return length
+                length = 0
+            else:
+                length += 1
+                i += 1
+        return length
