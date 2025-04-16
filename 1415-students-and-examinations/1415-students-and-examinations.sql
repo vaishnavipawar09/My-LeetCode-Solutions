e2ab83554f3b05992262e72718529f4d6fb5793c
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-04-16 10:46:02 -0400

    Time: 894 ms (88.92%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -5,4 +5,4 @@ CROSS JOIN Subjects sb
 LEFT JOIN Examinations e
 ON sb.subject_name = e.subject_name AND s.student_id = e.student_id
 GROUP BY s.student_id, s.student_name, sb.subject_name
-ORDER BY s.student_id, sb.subject_name
+ORDER BY s.student_id, sb.subject_name;
