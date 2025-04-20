a1fbd9245c730256d901b4b20190bca377d3114a
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-04-20 19:47:11 -0400

    Time: 760 ms (91.32%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -4,4 +4,4 @@ SELECT p.product_id,
 FROM Prices p
 LEFT JOIN UnitsSold s
 ON p.product_id = s.product_id AND s.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id
+GROUP BY p.product_id;
