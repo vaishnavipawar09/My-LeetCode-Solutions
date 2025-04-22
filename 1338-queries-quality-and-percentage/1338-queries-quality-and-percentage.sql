c6b2b2a51fa9e2a2530ab8b6c96579fc34faf8ad
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-04-22 08:50:22 -0400

    Time: 405 ms (61.32%), Space: 0B (100%) - LeetHub

--- 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -3,4 +3,4 @@ SELECT query_name,
        ROUND(AVG(rating * 1.0 / position), 2) AS quality, 
        ROUND((COUNT(CASE WHEN rating < 3 THEN 1 END) / COUNT(*)) * 100, 2) AS poor_query_percentage
 FROM Queries 
-GROUP BY query_name
+GROUP BY query_name;
