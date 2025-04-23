3e665fefebcc80bbcf593139fb62d6191dbabd6e
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-04-23 14:25:43 -0400

    Time: 613 ms (71.47%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -5,4 +5,4 @@ SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country,
     SUM(amount) AS trans_total_amount, 
     SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY month, country
+GROUP BY month, country;
