887e15b417523f734437b957eb357487db289378
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-01 15:10:49 -0400

    Time: 486 ms (32.29%), Space: 26.3 MB (37.93%) - LeetHub

---
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.py
@@ -0,0 +1,42 @@
+from sortedcontainers import SortedList
+
+
+class Solution:
+    def maxTaskAssign(
+        self, tasks: List[int], workers: List[int], pills: int, strength: int
+    ) -> int:
+        n, m = len(tasks), len(workers)
+        tasks.sort()
+        workers.sort()
+
+        def check(mid: int) -> bool:
+            p = pills
+            ws = deque()
+            ptr = m - 1
+            # Enumerate each task from largest to smallest
+            for i in range(mid - 1, -1, -1):
+                while ptr >= m - mid and workers[ptr] + strength >= tasks[i]:
+                    ws.appendleft(workers[ptr])
+                    ptr -= 1
+                if not ws:
+                    return False
+                # If the largest element in the deque is greater than or equal to tasks[i]
+                elif ws[-1] >= tasks[i]:
+                    ws.pop()
+                else:
+                    if p == 0:
+                        return False
+                    p -= 1
+                    ws.popleft()
+            return True
+
+        left, right, ans = 1, min(m, n), 0
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
