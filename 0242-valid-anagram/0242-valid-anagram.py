dc0b0a668eda6a372fb9b70e939c2fa39bb5ace2
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-27 18:00:35 -0400

    Time: 15 ms (42.77%), Space: 17.8 MB (63.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) != len(t):                        #Check if both the strings are of same size
             return False
         
-        cntS, cntT = {}, {}
+        cntS, cntT = {}, {}                         #Create Hashmap for both strings
 
-        for i in range(len(s)):
-            cntS[s[i]] = 1 + cntS.get(s[i], 0)
-            cntT[t[i]] = 1 + cntT.get(t[i], 0)
+        for i in range(len(s)):                     #Iterate through the string
+            cntS[s[i]] = 1 + cntS.get(s[i], 0)      #Increate the count of the charatcter using .get, s[i] is the key
+            cntT[t[i]] = 1 + cntT.get(t[i], 0)      # 0 is the default value
         return cntS == cntT
 
     
-        
+        #Time Comlexity: O(n + m),    Where n is the length of string s and m is the length of string t.
+        #Space Complexity: O(1)
