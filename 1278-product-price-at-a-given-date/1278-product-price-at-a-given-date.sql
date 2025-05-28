df6ec48bd345e46c981d0b0e2c2d76acb2288e61
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-28 10:45:16 -0400

    Time: 441 ms (92.1%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 SELECT p.product_id,
-       COALESCE(p1.new_price, 10) AS price
-FROM (SELECT DISTINCT product_id FROM Products) p
-LEFT JOIN (
+       COALESCE(p1.new_price, 10) AS price           #Checks if new_price is NULL, if yes assigns the 10 value
+FROM (SELECT DISTINCT product_id FROM Products) p    #Unique Product IDs, Need All Products
+LEFT JOIN (                                          #To preserve all product_ids
     SELECT product_id, new_price
     FROM Products
-    WHERE change_date <= '2019-08-16'
-    AND (product_id, change_date ) IN (
+    WHERE change_date <= '2019-08-16'                # Interested in on or before that date
+    AND (product_id, change_date ) IN (              #Inside IN, finds the latest price change date
       SELECT product_id, MAX(change_date)
       FROM Products
       WHERE change_date <= '2019-08-16'
