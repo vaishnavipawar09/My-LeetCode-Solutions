9f21ae613b470be9e38177455bf59c829d0ff01a
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-28 19:56:41 -0400

    Time: 0 ms (100%), Space: 18.9 MB (34.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -25,5 +25,22 @@ class Solution:
 Time Complexity : O(n^2), Space Complexity: O(1)
 
 This above is the Brute force method, iterate through the whole array to find the target"""
-        
-        
+
+"""
+Method 3: Hash Map 2 Pass:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices = {}  # val -> index
+
+        for i, n in enumerate(nums):     #This loop is to store all the values in the Hashmap
+            indices[n] = i
+
+        for i, n in enumerate(nums):     #Find if the values exits
+            diff = target - n
+            if diff in indices and indices[diff] != i:
+                return [i, indices[diff]]
+
+#Time Complexity: O(n) 
+#Space Complexity: O(n)
+
+"""
