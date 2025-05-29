0e69a0301da85c769abc11875ec332dbb6a691fd
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-29 13:07:39 -0400

    Time: 792 ms (72.45%), Space: 0B (100%) - LeetHub

--- 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
-WITH cte AS(
+WITH cte AS(                    #Ctreated a temporary table to calculate the total weight 
     SELECT person_name, turn, SUM(weight) OVER (ORDER BY turn) AS running_wt
     FROM Queue
 )
 SELECT person_name
 FROM cte
-WHERE running_wt <= 1000
-ORDER BY turn DESC 
-LIMIT 1;                            #To sort using turn on the table
+WHERE running_wt <= 1000        #Board people only within weight limit
+ORDER BY turn DESC              #To sort using turn on the table
+LIMIT 1;                        #Last person                            
 
