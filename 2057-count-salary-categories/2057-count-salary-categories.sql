00d38acd0d982870963b67cd30c51cfc36f4f06c
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-05-30 18:50:07 -0400

    Time: 1559 ms (51.07%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,13 +1,13 @@
 SELECT c.category, 
        COUNT(a.account_id) AS accounts_count
-FROM (
+FROM (                                  #Union to satisfy the three categories together
     SELECT 'Low Salary' AS category
     UNION ALL
     SELECT 'Average Salary'
     UNION ALL
     SELECT 'High Salary'
 ) c
-LEFT JOIN Accounts a
+LEFT JOIN Accounts a                                    #to satisfy the no accounts return 0 condition
   ON (c.category = 'Low Salary' AND a.income < 20000)
   OR (c.category = 'Average Salary' AND a.income BETWEEN 20000 AND 50000)
   OR (c.category = 'High Salary' AND a.income > 50000)
