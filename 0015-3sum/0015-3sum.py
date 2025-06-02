3e2fa0bcc97ec248d9db2659017c0ffa376ae800
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-01 20:52:14 -0400

    Time: 567 ms (64.66%), Space: 20.7 MB (37.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
+        res = []                                    #Return the result in the form of list hence []
+        nums.sort()                                 #Sort the array
 
-        for i, a in enumerate(nums):
-            if a > 0:
-                break
-
-            if i > 0 and a == nums[i - 1]:
+        i = 0                                       
+        for i in range(len(nums)):                  #Iterates through the array
+            if i > 0 and nums[i] == nums[i - 1]:    #If duplicates, move forward, i is at start 
                 continue
 
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-                        
-        return res
+            j, k = i + 1, len(nums) - 1             #Create left and right pointer, left is i +1, cause i!=j
+
+            while j < k:
+                calsum = nums[i] + nums[j] + nums[k] #Calculate the three sum
+                if  calsum < 0:                     #Sum < 0, increase the left pointer by 1
+                    j += 1
+                elif calsum > 0:                    #Sum > 0, decrease the right pointer by 1
+                    k -=1
+                else:                               #If sum == 0, add the values in the array
+                    res.append([nums[i], nums[j], nums[k]]) # add to te result
+                    j += 1                          #Update the pointers j and k
+                    k -= 1
+                    while j < k and nums[j] == nums[j - 1]: #Duplicate, so shift our pointer
+                        j += 1
+                    
+        return res                                  #return the result
+
+
+#Time Complexity: O(n^2)    (one loop takes nlog n and the other j, k loop takes n^2 so total it is n^2)
+#Space Complexity: O(1) or O(n) depends upon sorting algorithm
+
+    
