0f719a739a67da7446d3e8beee1d920f0394ba91
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-04 17:05:55 -0400

    Time: 103 ms (43.68%), Space: 27 MB (33.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+        
+        """
+        My version that I solved: Greedy approach:
         minprice = prices[0]  # Set the first price as the lowest seen so far (temp value to track min)
         maxprofit = 0         # Create a variable to store max profit found so far
 
@@ -12,3 +26,8 @@ class Solution:
                     maxprofit = profit  # Only update max profit if the new one is greater
 
         return maxprofit  # Return the best profit found
+
+        #Time Complexity: O(n)
+        #Space Complexity: O(1)
+
+        """
