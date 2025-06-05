e4f6d5355fee0357c2742865f3fc17fef5022675
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-05 17:39:26 -0400

    Time: 176 ms (30.31%), Space: 18.1 MB (48.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,6 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l = 0
+        count = {}
+        res = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+        return res
+
+    
+"""
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
         maxf = 0
         count = {}
         res = 0
@@ -16,3 +33,6 @@ class Solution:
             res = max(res, r - l + 1) # update result here
 
         return res
+
+"""
+
