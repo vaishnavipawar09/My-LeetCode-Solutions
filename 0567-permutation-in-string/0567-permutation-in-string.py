6d1d462a51aa1988c6aed2e2cdf83073c039c799
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-05 18:41:30 -0400

    Time: 15 ms (86.11%), Space: 17.8 MB (87.73%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,4 +1,5 @@
-"""class Solution:
+#Optimal Solution:
+class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):  # s1 can't be a permutation of a shorter s2
             return False
@@ -43,7 +44,10 @@
 
 #Time Complexity: O(n)   , n is len of s2 string
 #Space Complexity: O(1)
+
 """
+My solution:
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2): 
@@ -74,39 +78,7 @@ class Solution:
 
         return False  # No valid permutation found
 
-"""
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): 
-            return False
-
-        l=0
-        counts1= {}
-        counts2 = {}
-
-        for c in s1:       # Expand window
-            counts1[c] = 1 + counts1.get(c, 0)
-
-        for r in range(len(s2)):
-            counts2[s2[r]] = 1 + counts2.get(s2[r], 0)
-
-            while (r - l + 1) > len(s1):
-                counts2[s2[l]] -= 1
-                if counts2[s2[l]] == 0:  
-                    del counts2[s2[l]]
-                l += 1
-
-            if counts1 == counts2:
-                return True
-
-        return False                
-        
-#Time Complexity: O(n)   , n is len of s2 string
+#Time Complexity: O(26.n)   , n is len of s2 string
 #Space Complexity: O(1)
-        
-        
-        """
-        
-        
-   
-   
+
+"""
