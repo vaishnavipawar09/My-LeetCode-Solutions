079b6f425c6dff30ad4e2971e1c5f062d81d75df
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-18 16:59:47 -0400

    Time: 0 ms (100%), Space: 17.7 MB (61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,24 +3,32 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+#Iterative method - optimal 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        temp = ListNode()
-        tail = temp
+        dummy = ListNode()          #create a dummy list for output
+        tail = dummy                #tail will be dummy
         l1, l2 = list1, list2
 
         while l1 and l2:
-            if l1.val < l2.val:
+            if l1.val < l2.val:     #if list 1 value is less than list 2 value 
                 tail.next = l1      # attach l1’s node to tail
                 l1 = l1.next        # move l1 forward
-            else:
+            else:                   #if lit 2 is less than or equal than list 1
                 tail.next = l2
                 l2 = l2.next
-            tail = tail.next        # move tail forward
+            tail = tail.next        # move tail forward (update regardless of which node we insert into the list)
 
-        tail.next = l1 if l1 else l2
+        if l1:                      #if one is empty, then find the non empty lit and add it at the end of the list
+            tail.next = l1
+        else:
+            tail.next = l2    
         
-        return temp.next
+        return dummy.next           #return the dummy list
+
+#Time Complexity: O(n + m)
+#Space Complexity: O(1)
 
 
 
