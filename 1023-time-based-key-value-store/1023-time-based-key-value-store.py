c863e9e475cd2dcca212bd45d23102f330ef9d16
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-18 00:42:02 -0400

    Time: 79 ms (87.17%), Space: 73.7 MB (91.63%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -3,32 +3,36 @@ class TimeMap:
         self.store = {}  # key -> list of (timestamp, value)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
+        if key not in self.store:       #chcek if key exist in the store
             self.store[key] = []  # initialize with an empty list
-        self.store[key].append((timestamp, value))  # append the new (timestamp, value) pair
+        self.store[key].append((value, timestamp))  # append the new (timestamp, value) pair
      
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
+        if key not in self.store:       #if key did not exist return ""
             return ""
 
-        values = self.store[key]
-        l = 0
-        r = len(values) - 1
         res = ""
-
-        while (l <= r):
-            mid = (l + r ) // 2
-
-            if values[mid][0] <= timestamp:
-                res = values[mid][1]
-                l = mid + 1
-            else:
-                r = mid - 1
-           
-        return res
-
+        values = self.store.get(key, [])  #if find the match returns the list if not return empty []
+        l = 0                               #left ptr
+        r = len(values) - 1                 #right ptr at the end of list
         
 
+        while (l <= r):                     #if l hasn't crossed right run the loop
+            mid = (l + r ) // 2             #cal mid value
+
+     #compare mid is valyes, 0 is the timestamp, so check if it <= timestamp, to check if it exist
+            if values[mid][1] <= timestamp: 
+                res = values[mid][0]
+                l = mid + 1                 #search right portion
+            else:
+                r = mid - 1                 #search left portion of the list, 
+                                            #cant assign result here as it invalid value
+           
+        return res                          #return the result
+
+        
+#Time Complexity ; SET : O(1), GET : O(log n)
+#Space Complexity: O(m * n),  n = total no of values, m = total no. of keys
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
