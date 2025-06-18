0ac21346bedb68e758137a5737691b9e058090ec
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-18 18:01:39 -0400

    Time: 50 ms (46.17%), Space: 19.9 MB (56.48%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,17 +6,20 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
+        slow = head                 #use two ptrs, one slow and one fast assign both to head
         fast = head
 
-        if head is None or head.next is None:
-            return False
+        if head is None or head.next is None:#handle edge case where 1 or 0 node cant have a cycle
+            return False            #return false immediately , means no cycle
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        while fast and fast.next:   #We must make sure fast and fast.next are not none, if one is none, loop exits
+            slow = slow.next        #move slow by 1 space 
+            fast = fast.next.next   #move fast by 2 space
         
-            if slow == fast:
-                return True
-        return False
+            if slow == fast:        #if both are equal we get a cyclic linked list
+                return True         #return true if yes
+
+        return False                #no cyclic linked list found
         
+#Time Complexity: O(n)
+#Space Complexity: O(1)
