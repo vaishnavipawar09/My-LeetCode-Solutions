d5236a8aa1fbcb1a3d9bf58829e78b84c131eba0
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-20 14:53:23 -0400

    Time: 42 ms (51.45%), Space: 18.4 MB (84.2%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -6,7 +6,7 @@ class Node:
         self.next = next
         self.random = random
 """
-
+#Method 1 : Two pass Hash map technique
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if head is None:
@@ -14,21 +14,26 @@ class Solution:
 
         hashmap = {} #key, value
         
+        #First pass, creation the hashmap and start storing the values in it
         curr = head
-        while curr:
-            hashmap[curr] = Node(curr.val)
-            curr = curr.next
+        while curr:                         #run until the list end
+            hashmap[curr] = Node(curr.val)  #create hashmap and store the value of the node
+            curr = curr.next                #update the curr ptr until it reaches null
 
-        curr = head
-        while curr:
-            copy = hashmap[curr]
-            if curr.next:
-                copy.next = hashmap[curr.next]
-            if curr.random:
-                copy.random = hashmap[curr.random] 
-            curr = curr.next
+        #Second pass, start connecting the ptrs of the list, like original list
+        curr = head                         #start with the node
+        while curr:                         #run unti the list end
+            copy = hashmap[curr]            #create a copy and set the hashmap
+            if curr.next:                   #check if it empty or not
+                copy.next = hashmap[curr.next]  #map the original node to the copy next node
+            if curr.random:                 #check if it is empty or not
+                copy.random = hashmap[curr.random] #map the original node to the copy random node
+            curr = curr.next                #update the curr ptr until it reaches null
 
-        return hashmap[head]
+        return hashmap[head]                #return the head of the copy list 
+
+#Time Complexity: O(n)
+#Space Complexity : O(n)
 
 
         
