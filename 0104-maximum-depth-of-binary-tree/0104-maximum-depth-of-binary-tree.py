b4f7b3ad97c24ac7138aec5b8e7e56621e98f987
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-23 18:57:52 -0400

    Time: 0 ms (100%), Space: 19 MB (48.26%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+#Method 1 : Using Recursion
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:                #Handles all the base case, if the root is empty, there is no depth 
@@ -13,7 +16,36 @@ class Solution:
             rightdepth = self.maxDepth(root.right)          #Using recursion we found the depth of the right child
             return max(leftdepth, rightdepth) + 1         #max of left and right child depth, 1 is the depth of the root
 
-        
     #Time Complexity: O(n)
     #Space Complexity: O(n)
-        
+
+
+#Method 2 : Using Iterative DFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:                #Handles all the base case, if the root is empty, there is no depth 
+            return 0 
+
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+
+'''
+#Method 3 : Using BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:                #Handles all the base case, if the root is empty, there is no depth 
+            return 0
+
+        level = 0                  #our curr level is zero
+        q = deque([root])
+        while q:                    #until the queue is empty
+            for i in range(len(q)): #Traverse the entire level in the q
+                node = q.popleft()  #Pop from the left of the node and add its children
+                if node.left:            #Add left child, if node.left is not empty
+                    q.append(node.left)
+                if node.right:           #Add right child, if node.left is not empty
+                    q.append(node.right)
+            level += 1              #increase the level
+        return level                #if queue is empty return the total no. of level
+#Time Complexity: O(n)
+#Space Complexity: O(n)
