7187506e0016b6207d7409218043ea6422a0944f
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-23 14:46:56 -0400

    Time: 0 ms (100%), Space: 17.8 MB (32.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
-        return k
+        k = 0                           #Intialize k to track the total elements
+        for i in range(len(nums)):      #iterate through the whole array
+            if nums[i] != val:          #if the curr value doesnt match the required value, iterate
+                nums[k] = nums[i]       #Store the non target element at the curr index
+                k += 1                  #increment the index by 1, move to the next position
+        return k                        #return the total count of elements in the array , without val
         
+#Time COmplexity: O(n)
+#space Complexity: O(1)
