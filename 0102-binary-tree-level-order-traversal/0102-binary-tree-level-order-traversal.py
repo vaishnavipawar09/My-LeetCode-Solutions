7b2c70b138b2c2136b5dedfd70373238a3e850a2
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-24 20:33:36 -0400

    Time: 0 ms (100%), Space: 18.5 MB (38.62%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,26 +6,29 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
+        if root is None:        #handle base case if root is none
             return []
         
-        res = []
-        q = collections.deque()
-        q.append(root)
+        res = []                #create one to store result
+        q = collections.deque() #initailize the queue
+        q.append(root)          #first value to be added to the queue is the root value
 
-        while q:
-            qlen = len(q)
-            level = []
-            for i in range(qlen):
-                node = q.popleft()
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
+        while q:                #while q is not empty, iterates through the whole tree
+            qlen = len(q)       #cal the length of the queue
+            level = []          #temp var to calculate the level 
+            for i in range(qlen):   #itearate through the whole queue
+                node = q.popleft()  #as it is FIFO, we always pop from left, pop the node out and add its children 
+                if node:            #if the node is not empty and has children append it to the queue
+                    level.append(node.val)  #Update the level if adding the children
+                    q.append(node.left)     #add the left and right child nodes to the queue
                     q.append(node.right)
-            if level:
+            if level:               #if level is not empty, update the res with the level
                 res.append(level)
             
-        return res
+        return res              #return the max level
+
+#Time Complexity: O(n)
+#Space Complexity: O(n), cause we are a=using queue
 
 
         
