5297832594290e514aeb0ac8b9a7b89c83721e5a
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-06-28 14:55:28 -0400

    Time: 0 ms (100%), Space: 17.8 MB (60.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,24 @@
+#Here we iterate from the last of the array
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1                  #Start the ptr at the last of the array
+        length = 0                      #cal the length of last word
+
+        while s[i] == ' ':              #If there is space at the end before the word skip trailing spaces
+            i -= 1                      #Decrement the ptr to find the where the word ends?
+
+        while i >= 0 and s[i] != ' ':   #If i is at some word, and also if end of the string exit the loop, and char is not a space
+            length += 1                 #Count the length of the last word
+            i -= 1                      #Decrement the ptr to cal the length until you hit a space
+
+        return length                   #return the length
+
+            
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+
+        '''
+        #Here we iterate from the start itself
         length = i = 0
         while i < len(s):
             if s[i] == ' ':
@@ -12,3 +31,4 @@ class Solution:
                 length += 1
                 i += 1
         return length
+        '''
