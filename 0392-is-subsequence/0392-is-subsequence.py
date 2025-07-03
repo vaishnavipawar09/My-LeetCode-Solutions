e52b6ee6362038e49c209f5e0b4e8230d64aee92
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-03 11:35:31 -0400

    Time: 0 ms (100%), Space: 17.9 MB (42.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -11,4 +11,17 @@ class Solution:
 #Space Complexity: O(1)
 
 #Once i reaches out of bound we are done with the while loop
-#Also given s is not a subsequence of t
+#Also given s is a subsequence of t, and not the other way around
+
+#We are iterating and increamenting the s str cause, once that is over we dnt have to check anything cause s is subsequence of t, and not the other way around
+
+#DRY RUN
+# s = abc, t = ahbgdc
+# 1. i = j = 0, 0< 3 and 0< 6 yes, s[0] == t[0] -> a == a , yes, i = 1, j = 1
+# 2. i = j = 1, 1<3, 1< 6, s[1] == t[1] -> b == h does not satisfy so j +1, j = 2
+# 3. i = 1, j = 2, 1<3, 2< 6 , s[1] == t[2] -> b == b, yes, i=2, j = 3
+# 4. i =2, j = 3, 2< 3, 3< 6, s[2] == t[3] ->g no, increment j , j =4
+# 5. i = 2, j = 4. 2< 3, 4< 6, s[2] == t[4] -> c ==d, no , j = 5
+# 6. i =2, j =5, 2< 3, 5<6, s[2] == t[5] -> c == c , yes, i = 3, j = 6
+# 7. i = 3, j = 6 3 < 3 no end the while loop , return i == len(s)  3 ==3 yes, return True
+#Output: True
