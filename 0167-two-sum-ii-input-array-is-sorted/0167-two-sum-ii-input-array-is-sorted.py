acc2ff4f9585a3c84165e7418f92a11c4866483a
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-07 13:19:06 -0400

    Time: 4 ms (48.14%), Space: 18.7 MB (9.5%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -14,5 +14,12 @@ class Solution:
 #Time Complexity: O(n)    Loops through the array of n intergers
 #Space Complexity: O(1)   No extra space needed
 
-     
-        
+      
+#Dry Run, numbers = [2, 7, 11, 15], target = 9
+# 1. l = 0, r = 3, 0< 3 yes, currsum = 17 , 17 ==9 no, elif 17 < 9 no, else r = 2
+# 2. l = 0, r = 2, 0< 2 yes, currsum = 13,13 == 9 no, elif 13< 9 no , else r = 1
+# 3. l = 0, r = 1, 0< 1 yes, currsum = 9 , 9 == 9 yes, return [1, 2]
+
+# With -ve and 0 , numbers = [-1, 0, 1] Target = -1
+# 1. l = 0, r = 2, 0< 2 yes, currsum = 0, 0 ==-1 no, 0< -1 no , else r = 1
+# 2. l = 0, r = 1, 0< 1 yes, currsum = -1, -1 == -1 yes, return [1, 2]
