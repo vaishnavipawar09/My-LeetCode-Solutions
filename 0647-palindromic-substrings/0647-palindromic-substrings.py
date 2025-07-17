7af169f9dca2c0eea158f3429219ddf22fbe131e
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-16 23:02:51 -0400

    Time: 126 ms (76.98%), Space: 17.9 MB (34.39%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,4 +1,24 @@
 class Solution:
+    
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+
+        for i in range(len(s)):
+            res += self.countPali(s, i, i)          #here passed the left and right ptr for odd that is i, i    
+            res += self.countPali(s, i, i + 1)      #here passed the left and right ptr for eventhat is i, i +1   
+        return res
+
+    def countPali(self, s, l, r):           #Same as below code, just created a helper function the reduce the lies of the code
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
+       
+       
+"""
+class Solution:
     def countSubstrings(self, s: str) -> int:
         # Initialize the result counter
         res = 0
@@ -48,3 +68,4 @@ class Solution:
         - "For each position (and each gap), I grow outwards, and count palindromes."
         - "Single letters, two letters, and longer substrings are all handled."
         '''
+"""
