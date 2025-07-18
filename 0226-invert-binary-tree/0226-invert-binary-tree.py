fd4e077419755aaec79c1f98ebe1b0f44d533a2a
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-18 16:43:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (50.14%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,4 +1,4 @@
-"""
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -19,10 +19,35 @@ class Solution:
         return root                 #Return the root (tree)
 
 #Time Complexity: O(n)
-#Space Complexity: O(n)
-        
+#The function starts at the root., For every node, it swaps its left and right children (O(1))., a constant amount of work
+#Then it makes recursive calls on the left and right children., This process continues until all nodes are visited.
+#No node is visited more than once, So if there are n nodes, the function runs in O(n) time.
+
+#Space Complexity: O(n) = O(h), h = height of the tree.
+#In the worst case (a skewed tree, like a linked list), h = n → so O(n).
+#In the best/average case (a balanced tree), h = log n → O(log n).
+#his is due to the recursion stack,At any moment, the maximum number of function calls in the stack is equal to the height of the tree.
+
+#Dry Run
+#[4, 2, 7, 1, 3, 6, 9]
+# 1. root = 4, root.left = 7, root.right = 2  [4, 7, 2, 1, 3, 6, 8]
+# 2. root = 4, root.left invertTree(7), root.left= 9 root.right = 6
+# 3. invertTree(9) None, no effect, rturn node 9
+# 4. invertTree(6) None, no effect, return node 6
+# 5. invertTree(2), root.left = 3, root.right = 1
+# 6. invertTree(3), none, no effect , rturn 3
+# 7. invertTree(1), none, no effect, return 1
+# 8. root = [4, 7, 2, 9, 6, , 3, 1]
 
 
+# [1,2, 3]->[1, 3, 2]
+# root = 1, temp =2, root.left = 3, root.right = 2
+# root.left = 3 
+# root.right = 2
+# root = [1, 3, 2]
+
+"""   
+#Self practice:
 #Clarifying questions:
 #Is the tree a standard binary tree or could it be a bst? binary tree
 #can the tree be empty and have only one node? yes
@@ -38,17 +63,32 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right 
-"""
+#DFS
+class Solution:
+    def invertTree(self, root:Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+
+        root.left , root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+#BFS
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root is None:
             return root
 
-        temp = root.left
-        root.left = root.right
-        root.right = temp
+           
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left , node.right = node.right, node.left
 
-        self.invertTree(root.left)
-        self.invertTree(root.right)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return root
-        
+"""
