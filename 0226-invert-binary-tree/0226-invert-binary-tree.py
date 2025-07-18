5c7a8d0a451a74eabd63436299ca509af05a3a34
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-18 16:14:10 -0400

    Time: 0 ms (100%), Space: 17.7 MB (50.14%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,3 +1,4 @@
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -19,4 +20,35 @@ class Solution:
 
 #Time Complexity: O(n)
 #Space Complexity: O(n)
+        
+
+
+#Clarifying questions:
+#Is the tree a standard binary tree or could it be a bst? binary tree
+#can the tree be empty and have only one node? yes
+#should i invert the tree in place or create a new tree and return the root? inplace is more efficient 
+#are there any constraints on the node value? 0 to 100
+#what should i return if the tree is empty? null or empty root? []
+
+#Tree [1, 2, 3] _> [1, 3, 2]
+# I notice for every node, we simply swap its left and right children. We can do this recursively (DFS), or iteratively using a queue (BFS). Both work since the tree isn't large.
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, roiht = None):
+        self.val = val
+        self.left = left
+        self.right = right 
+"""
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
         
