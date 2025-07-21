68e810bc3b976020eeacd78753667a856d1de473
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-21 10:12:35 -0400

    Time: 320 ms (9.33%), Space: 25.5 MB (22.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,6 +1,39 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r, c))
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    nr, nc = row +dr, col +dc
+                    if (nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1" and (nr, nc) not in visited):
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        return islands
+
+
+"""
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
             return 0                       # Edge case: If grid is empty, return 0
 
         rows, cols = len(grid), len(grid[0])   # Get number of rows and columns
@@ -73,11 +106,13 @@ class Solution:
 # 4. Iterate Over Grid: For every cell: If the cell is land ('1') and not visited, it's a new island. Call bfs to visit the entire island. Increment the island count.
 # 5. Return Result: Return the total number of islands found.
 
-#implemenattaion in short 
+#implemenataion in short 
 # 1. Iterate over every cell in the grid.
 # 2. If cell is land and not visited, run BFS from there to visit the entire island.
 # 3. Use a visited set to ensure no land cell is counted more than once.
 # 4. Count how many times BFS is started; this is the number of islands.
 
-
-        
+#Clarifying questions:
+#1. Can i move diagonally ? No
+#
+  """      
