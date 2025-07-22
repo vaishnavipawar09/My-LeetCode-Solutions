1e1f4421b4925012dc92cb90f254eb646bc1f34f
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2025-07-22 18:39:03 -0400

    Time: 0 ms (100%), Space: 17.6 MB (82.65%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -12,9 +12,18 @@ class Solution:
 
 #Dry Run
 # nums = [3, 2, 2, 3], val = 3, k = 0
-# 1. k = 0, i = 0, nums[i] != val ie nums[0] !=val ie 3 != 3 no so break loop and increase i i = 1
+# 1. k = 0, i = 0, nums[i] != val ie nums[0] !=val ie 3 != 3 no, they are equal so break loop and increase i i = 1
 # 2. k = o, i = 1, nums[1] !=val ie, 2 != 3 yes, nums[k] = nums [i] ie nums[0] = nums[1] = nums[0] = 2, k =1, i =2  nums= [2,]
 # 3. k =1, i =2, nums[2] !=val ie 2 != 3 yes, nums[1] = nums[2], nums[1] = 2, k =2, i = 3   nums = [2, 2]
-# 4. k = 2, i =3, nums[3] ! = val, 3!=3 no, break , i =4        
+# 4. k = 2, i =3, nums[3] ! = val, 3!=3 no both are equal, break , i =4        
 # 5. i = 4 the loop doesnt execute cause it is over 
 # 6. return k , currently k = 2 return k Output is 2
+
+"""
+#Clarifying Questions
+Can I use extra space?No, must be in-place.
+Is the array always sorted?Yes, non-decreasing.
+What do I return?Return the count of unique elements (k).
+Do I need to care about what is in nums after the first k elements?No, can be anything.
+Does it contain duplicates Yes
+"""
