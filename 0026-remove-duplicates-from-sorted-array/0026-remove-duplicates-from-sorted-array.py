cbb181a81c347f1c608e3c2b73fee9d64a640ac3
Vaishnavi Pawar <88104401+vaishnavipawar09@users.noreply.github.com>
2026-02-12 12:20:38 -0500

    Time: 0 ms (100%), Space: 20.6 MB (26.32%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,13 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = 1                           #Intitilize k = 1, since first element is always unique
-
-        for i in range(len(nums)):      #iterate through the whole array
-            if nums[i] != nums[k - 1]:  #if nums[i] is unique then the last value we have seen  
+        k = 1                            #Intitilize k = 1, since first element is always unique
+        for i in range(1, len(nums)):   #Iterate through the array staring from 1, to the end
+            if nums[i] != nums[i - 1]:  #Compare with the prev index as the array is already sorted
                 nums[k] = nums[i]       #Store the non target element at the curr index
                 k += 1                  #increment the index by 1, move to the next position
         return k                        #return the total count of elements in the array , without val
-        
+
 #Time Complexity: O(n)
 #Space Complexity: O(1)
 
@@ -16,6 +15,19 @@ class Solution:
 # 2. k =1, i = 1, nums[1] ! = nums[0], 1 !=1 no, i = 2, nums = [1, 1, 2]
 # 3. k = 1, i = 2, nums[2] != nums[0], 2 != 1, yes, nums[1] = nums[2] = 2, k = 2 nums = [1, 2 , 2]
 # 4. k =2, i = 3, the max length was 2 , not allowed, k = 2, output is k = 2
+
+"""
+Method 2:
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1                           #Intitilize k = 1, since first element is always unique
+
+        for i in range(len(nums)):      #iterate through the whole array
+            if nums[i] != nums[k - 1]:  #if nums[i] is unique then the last value we have seen  
+                nums[k] = nums[i]       #Store the non target element at the curr index
+                k += 1                  #increment the index by 1, move to the next position
+        return k                        #return the total count of elements in the array , without val
+ """       
 
 
 """
